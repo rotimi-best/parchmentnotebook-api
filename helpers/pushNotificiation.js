d6b37a3fa7d3954a5ffd432b9a17b4082feddddb
Rotimi Best <rotimiibitoyeemma@gmail.com>
2023-06-17 21:55:34 +0100

    Pause notification 2

--- helpers/pushNotificiation.js
+++ helpers/pushNotificiation.js
@@ -1,38 +1,38 @@
-const webPush = require('web-push');
-const {
-  clientUrl,
-  adminEmail,
-  publicVapidKey,
-  privateVapidKey
-} = require('../config');
+// const webPush = require('web-push');
+// const {
+//   clientUrl,
+//   adminEmail,
+//   publicVapidKey,
+//   privateVapidKey
+// } = require('../config');
 
-webPush.setVapidDetails(`mailto:${adminEmail}`, publicVapidKey, privateVapidKey);
+// webPush.setVapidDetails(`mailto:${adminEmail}`, publicVapidKey, privateVapidKey);
 
-/**
- * Send push notitification to device
- * @param {object} subscription Device subscription data
- * @param {object} data Notification options: title*, body*, url
- */
-const sendPush = async (subscription, data, onError) => {
-  // Create payload - title, body are required
-  const payload = JSON.stringify({
-    icon: `${clientUrl}/images/icons/icon-128x128.png`,
-    url: `${clientUrl}/prayers`,
-    ...(data || {})
-  });
+// /**
+//  * Send push notitification to device
+//  * @param {object} subscription Device subscription data
+//  * @param {object} data Notification options: title*, body*, url
+//  */
+// const sendPush = async (subscription, data, onError) => {
+//   // Create payload - title, body are required
+//   const payload = JSON.stringify({
+//     icon: `${clientUrl}/images/icons/icon-128x128.png`,
+//     url: `${clientUrl}/prayers`,
+//     ...(data || {})
+//   });
 
-  try {
-    await webPush.sendNotification(subscription, payload);
-  } catch (error) {
-    console.error('Error while sending push')
-    if (onError && typeof(onError) === "function") {
-      onError(error)
-    }
-  }
+//   try {
+//     await webPush.sendNotification(subscription, payload);
+//   } catch (error) {
+//     console.error('Error while sending push')
+//     if (onError && typeof(onError) === "function") {
+//       onError(error)
+//     }
+//   }
 
-  return null;
-}
+//   return null;
+// }
 
-module.exports = {
-  sendPush
-}
+// module.exports = {
+//   sendPush
+// }
